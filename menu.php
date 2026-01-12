b9ea513eabfc69b620aaaebf4cec7fed105f4639
emilalexe <emil.alexe92@gmail.com>
2026-01-12 23:18:28 +0200

    Update menu.php
    Navbar

--- menu.php
+++ menu.php
@@ -1,4 +1,4 @@
-<nav>
+<nav class="btn-group" role="group">
     <a class="navbar-brand logo" href="./" aria-label="MailBox"><?=getLogo() ?></a>
     <li>
         <a href="./">Home</a>
@@ -6,4 +6,33 @@
     <li>
         <a href="./about">About</a>
     </li>
+</nav>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand logo" href="./" aria-label="MailBox"><?=getLogo() ?></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="./">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./about">About</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="./price" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Prices
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="./price#1">SmartBox #1</a></li>
+            <li><a class="dropdown-item" href="./price#2">SmartBox #2</a></li>
+            <li><a class="dropdown-item" href="./price#3">SmartBox #3</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
 </nav>
